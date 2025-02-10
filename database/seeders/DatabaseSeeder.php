d86bb00e0a1795227fc5c8f65f2d268215742dd1
Wafiqah Saat <wafiqahsaat@gmail.com>
2025-02-10 22:13:32 +0800

    first push to Main

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        //     'name' => 'Admin',
+        //     'email' => 'admin@gmail.com',
+        //     'password' => 'admin123'
         // ]);
     }
 }
